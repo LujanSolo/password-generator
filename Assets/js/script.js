5807a0ed232e03a37262d194002e55aeecc7a122
LujanSolo <lujansolo@proton.me>
2022-08-19 23:36:30 -0700

    renamed variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,18 @@
   // at start, ask user how many characters they want in password
-var numChars = prompt("How many characters would you like your password to be? Note: Password length must be at least 8 and no more than 128 characters.");
+var passwordLength = prompt("How many characters would you like your password to be? Note: Password length must be at least 8 and no more than 128 characters.");
 
 //validate that they've got at least that much right
-if (numChars < 8 || numChars > 128) {
+// function checkUserInput(numChars) {
+    //   parseInt(chars)?
+    //   if (chars < 8 || chars > 128) { 
+    //   return null
+    //    }
+    //  }
+if (passwordLength < 8 || passwordLength > 128) {
   alert("Please select a number between \n8 and 128 \n for password length");
 }
+
+
 //  VARIABLES needed -  
 //      -characters of lowercase, upper, numeric, and special
 var upperChars = confirm("Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO");
@@ -15,27 +23,20 @@ var specialChars = confirm("Would you like to include special characters? \nOK f
 
 //  an empty array to hold user's selected characters
 var charOptions = [];
+
+//  CHECK to see that at least 1 option is TRUE
+if (!specialChars && !numberChars && !lowerChars && !upperChars)alert("Choose at least one character type for your password. We believe in you.")
+    //     START OVER return to start variable
   
 //    then, CHECK to see if all user entries are valid (function) 
-function validateInput (upperChars) {}
+function validateInput (upperChars) {
+
+}
 
 
-// function checkUserInput(numChars) {
-    //   parseInt(chars)?
-    //   if (chars < 8 || chars > 128) { 
-    //   return null
-    //    }
-    //  }
 
-    // THEN make VARIABLES for types of characters
-    //  var specialChars = confirm("Want special characters in your password?")
-    //  var numberChars = confirm("Want numbers in your password?")
-    //  var lowerChars = confirm("Want lowercase characters in your password?")
-    //  var upperChars = confirm("Want uppercase characters in your password?")
-    //  CHECK to see that at least 1 option is TRUE
-    //  or if (!specialChars && !numberChars && !lowerChars && !upperChars) {
-    //   alert("Choose at least one character type for your password. We believe in you.")
-    //     START OVER return to start variable
+  
+    
     
     //  once validates, push info to array (charOptions)
     // if (specialChars) {
