a08de1491a3cfbb31048bf158e693ddd4978ac86
LujanSolo <lujansolo@proton.me>
2022-08-20 14:45:36 -0700

    rearranged order so that user input doesn't continue if user puts an invalid number for amount of characters

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
 // Assignment code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
-
+  var password = generatePassword();
 // Add event listener to generate button
-
 
 // Write password to the #password input
 
+// all possible variables here:
+var isLower = "abcdefghijklmnopqrstuvwxyz".split("");
+var isUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var isSpecial = " !@#$%^&*()_-+=\"'{/}:;?>.<,".split("");
+var isNumber = "1234567890".split("");
+var minChars = 8;
+var maxChars = 128;
 
-  // trying to put most everything inside of this function below;
+var masterArray = [isLower, isUpper, isSpecial, isNumber];
 
-  //  VARIABLES needed -
-  //      -characters of lowercase, upper, numeric, and special
-  // var upperChars = confirm("Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO");
-  // var lowerChars = confirm("Would you like to include lower case letters? \nOK for YES, Cancel for NO");
-  // var  numbers = confirm("Would you like to include numbers in your password? \nOK for YES, Cancel for NO");
-  // var specialChars = confirm("Would you like to include special characters? \nOK for YES, Cancel for NO");
+function generatePassword() {
+  var allInput = "";
+  var passwordLength = parseInt(
+    prompt(
+      "How long would you like your password to be? Be sure to pick a number between 8 and 128."
+    )
+  );
+  if (passwordLength < minChars || passwordLength > maxChars) {
+    prompt.passwordLength;
+    return alert("Please select a number between 8 and 128.");
+  };
+  
+  var lowerChars = confirm(
+    "Would you like to include lower case letters? \nOK for YES, Cancel for NO"
+  );
+  var upperChars = confirm(
+    "Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO"
+  );
+  var specialChars = confirm(
+    "Would you like to include special characters? \nOK for YES, Cancel for NO"
+  );
+  var numberChars = confirm(
+    "Would you like to include numbers in your password? \nOK for YES, Cancel for NO"
+  );
 
-
-    // all possible variables here:
-    var isLower = "abcdefghijklmnopqrstuvwxyz".split("");
-    var isUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    var isSpecial = " !@#$%^&*()_-+=\"'{/}:;?>.<,".split("");
-    var isNumber = "1234567890".split("");
-    var minChars = 8;
-    var maxChars = 128;
-
-    var masterArray = [isLower, isUpper, isSpecial, isNumber];
-    var password = "";
-
-    
-    function generatePassword() {
-      var passwordLength = parseInt(prompt("How long would you like your password to be? Be sure to pick a number between 8 and 128."));
-      var lowerChars = confirm("Would you like to include lower case letters? \nOK for YES, Cancel for NO");
-      var upperChars = confirm("Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO");
-      var specialChars = confirm("Would you like to include special characters? \nOK for YES, Cancel for NO");
-      var numberChars = confirm("Would you like to include numbers in your password? \nOK for YES, Cancel for NO");
-      
-      if (passwordLength < minChars || passwordLength > maxChars){
-        prompt.passwordLength;
-        return alert("Please select a number between 8 and 128.")}
-
-      if (!specialChars && !numberChars && !lowerChars && !upperChars)
-        alert(
-          "Choose at least one character type for your password. We believe in you..."
-        );
-
-        console.log("hello");
-      // return generatePassword();
-
-    // Now, in the same function, test which character types user selected and add them to a string
-    if (lowerChars || upperChars || specialChars || numberChars) {
-      if (lowerChars) {
-        password.concat.isLower;
-      }
-      if (upperChars) {
-        password.concat.isUpper;
-      }
-      if (specialChars) {
-        password.concat.isSpecial;
-      }
-      if (numberChars) {
-        password.concat.isNumber;
-      }
-    return generatePassword();
-    
-    // now, the var password contains all characters based on user's choices. USE math.method to randomly select characters from VAR PASSWORD based on the number of CHARACTERS the user chose:
-    function randoPass() {
-      for (var i = 0; i < password.length; i++) {
-        masterArray.push(password[i]);
-      }
-      function writePassword() {
-        var password = generatePassword();
-        passwordText.textContent = password;
-      }
+  if (lowerChars || upperChars || specialChars || numberChars) {
+    if (lowerChars) {
+      allInput.concat.isLower;
+    };
+    if (upperChars) {
+      allInput.concat.isUpper;
+    };
+    if (specialChars) {
+      allInput.concat.isSpecial;
+    };
+    if (numberChars) {
+      allInput.concat.isNumber;
     }
+  };
+  
+  function writePass() {
+    randoSelects = [Math.floor(Math.random() * masterArray)];
+    passwordText.value = password;
   }
-  // generateBtn.addEventListener("click", writePassword);
 }
+
+generateBtn.addEventListener("click", generatePassword);
+
+  // console.log("hello");
+  // return generatePassword();
+
+  // Now, in the same function, test which character types user selected and add them to a string
+
+  // return generatePassword();
+
+  // // now, the var password contains all characters based on user's choices. USE math.method to randomly select characters from VAR PASSWORD based on the number of CHARACTERS the user chose:
+  // function randoPass() {
+  //   for (var i = 0; i < password.length; i++) {
+  //     masterArray.push(password[i]);
+  //   }
+
+  // }
 
