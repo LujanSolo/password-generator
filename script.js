52eb925b46c92239e56aa35e94db09740a2118d2
LujanSolo <lujansolo@proton.me>
2022-08-20 21:15:13 -0700

    document cleanup

--- script.js
+++ script.js
@@ -2,43 +2,30 @@
 var generateBtn = document.querySelector("#generate");
 
 // List of all possible characters for password, masterArray to hold random selections, character min/max
+
 var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialLetters = " !@#$%^&*()_-+=\"'{/}:;?>.<,";
 var numberChars = "1234567890";
-
-// var lowerLetters = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
-// var isUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var isSpecial = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-// var isNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-// var masterArray = [lowerLetters, isUpper, isSpecial, isNumber];
-
 var minChars = 8;
 var maxChars = 128;
 
 // function must go through prompts, resetting user if character number is invalid
 function generatePassword() {
-  console.log(3);
   var possibleChars = "";
-
-  // A SERIES OF PROMPTS TO LEAD USER THRU PASSWORD INPUT OPTIONS:
   var passwordLength = parseInt(
     prompt(
       "How long would you like your password to be? Be sure to pick a number between 8 and 128."
     )
   );
+
   // if clause to check if password length is valid
   // put in clause to restrict to numbers only
-
-  console.log(typeof passwordLength);
-
   if (
     passwordLength < minChars ||
     passwordLength > maxChars ||
     isNaN(passwordLength)
   ) {
-    console.log(5);
     alert("Invalid password length entered.");
     return;
   }
@@ -56,8 +43,10 @@ function generatePassword() {
   var isNumberChars = confirm(
     "Would you like to include numbers in your password? \nOK for YES, Cancel for NO"
   );
-  var password = "";
 
+  var password = ""; // password character storage
+
+  //function below concats random characters from each var to the current password data
   if (isLowerChars) {
     password += lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
   }
@@ -77,8 +66,8 @@ function generatePassword() {
     alert("Please choose at least one character style.");
     return;
   }
-  console.log(1);
-  // if (lowerChars || upperChars || specialChars || numberChars) {
+
+  // and add each type of characters that the user selected:
   if (isLowerChars) {
     possibleChars += lowerLetters;
   }
@@ -92,17 +81,15 @@ function generatePassword() {
     possibleChars += numberChars;
   }
   console.log(possibleChars);
-  var currentPasswordLength = password.length;
 
+  // variable to include in formula below to offset "extra" characters in the password field, as *above* formula will add 1 for each type selected, automatically. So, it needs to be as below:
+  var currentPasswordLength = password.length;
   for (i = 0; i < passwordLength - currentPasswordLength; i++) {
     password += possibleChars[Math.floor(Math.random() * possibleChars.length)];
-    // console.log(possibleChars.length);
   }
   console.log(password);
   return password;
 }
-
-console.log(2);
 
 function getRandom(arr) {
   var randIndex = Math.floor(Math.random() * arr.length);
