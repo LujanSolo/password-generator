fe27676be5ec3b995113f5842f6e97b47442f669
LujanSolo <lujansolo@proton.me>
2022-08-20 18:37:10 -0700

    refined if statement at line 50

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var isSpecial = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "."
 var isNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
-var masterArray = [isLower, isUpper, isSpecial, isNumber];
+// var masterArray = [isLower, isUpper, isSpecial, isNumber];
 
 var minChars = 8;
 var maxChars = 128;
@@ -23,13 +23,14 @@ var maxChars = 128;
 // function must go through prompts, resetting user if character number is invalid
 function generatePassword() { 
 
-  var possibleChars = [];
+  var possibleChars = "";
   
 
   // A SERIES OF PROMPTS TO LEAD USER THRU PASSWORD INPUT OPTIONS:
   var passwordLength = parseInt(prompt("How long would you like your password to be? Be sure to pick a number between 8 and 128."));
   // if clause to check if password length is valid
-  if (passwordLength < minChars || passwordLength > maxChars)
+  // put in clause to restrict to numbers only
+  if (passwordLength < minChars || passwordLength > maxChars) 
       return alert("Please select a number between 8 and 128."),
       prompt.passwordLength;
 // PROMPTS CONTINUED...
@@ -38,43 +39,39 @@ function generatePassword() {
   var specialChars = confirm("Would you like to include special characters? \nOK for YES, Cancel for NO");
   var numberChars = confirm("Would you like to include numbers in your password? \nOK for YES, Cancel for NO");
 
-  // AN INDEX FOR THE PASSWORD
-  var password = [];
+  // store password here
+  var password = "";
 
   // if clause to see if user chose at least one character type:
   if (!lowerChars && !upperChars && !specialChars && !numberChars) {
     alert("Please choose at least one character style. We're wasting time, here...");
     return generatePassword();}
-    elseif (lowerChars || upperChars || specialChars || numberChars)
-      if (lowerChars) {
-        possibleChars = possibleChars.concat(isLower);
-      }   if (upperChars) {
-        possibleChars = possibleChars.concat(isUpper);
-      }   if (specialChars) {
-        possibleChars = possibleChars.concat(isSpecial);
-      }   if (numberChars) {
-        possibleChars = possibleChars.concat(isNumber);
+
+  if (lowerChars || upperChars || specialChars || numberChars)
+      if (lowerChars) {possibleChars.concat(isLower);
+      }
+      if (upperChars) {possibleChars.concat(isUpper);
+      }   if (specialChars) { possibleChars.concat(isSpecial);
+      }   if (numberChars) {possibleChars.concat(isNumber);
       }
       console.log(possibleChars);
-      for (i=0; i<passwordLength.length; i++) {
-      password = password.concat(randoChars(possibleChars));
+      for (i=0; i<passwordLength; i++) {
+      password = password.concat( getRandom(possibleChars));
      }
     return password
-  }   
-
-
-function randoChars(arr) {
-  var randIndex = Math.floor(Math.random() * arr.length);
-  var randElement = arr[randIndex];
-  return randElement;}
-
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-    
   }
+  
+  function getRandom(arr) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+    var randElement = arr[randIndex];
+    return randElement;
+  }  
+    
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+  
+      passwordText.value = password;
+}
 
-// console.log("heya");
 generateBtn.addEventListener("click", generatePassword);
