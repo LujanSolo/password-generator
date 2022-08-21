1ce0e47bc078fc293a86cab63af113620cc7b68d
LujanSolo <lujansolo@proton.me>
2022-08-20 20:42:15 -0700

    corrected broken functions, fixed errored brackets, improved flow for functionality on site

--- script.js
+++ script.js
@@ -1,77 +1,120 @@
 // Assignment code
 var generateBtn = document.querySelector("#generate");
 
-
 // List of all possible characters for password, masterArray to hold random selections, character min/max
-// var isLower = "abcdefghijklmnopqrstuvwxyz";
-// var isUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var isSpecial = " !@#$%^&*()_-+=\"'{/}:;?>.<,";
-// var isNumber = "1234567890";
+var isLower = "abcdefghijklmnopqrstuvwxyz";
+var isUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var isSpecial = " !@#$%^&*()_-+=\"'{/}:;?>.<,";
+var isNumber = "1234567890";
 
-
-var isLower = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
-var isUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var isSpecial = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-var isNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
+// var isLower = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
+// var isUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var isSpecial = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+// var isNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // var masterArray = [isLower, isUpper, isSpecial, isNumber];
 
 var minChars = 8;
 var maxChars = 128;
-  
-// function must go through prompts, resetting user if character number is invalid
-function generatePassword() { 
 
+// function must go through prompts, resetting user if character number is invalid
+function generatePassword() {
+  console.log(3);
   var possibleChars = "";
-  
 
   // A SERIES OF PROMPTS TO LEAD USER THRU PASSWORD INPUT OPTIONS:
-  var passwordLength = parseInt(prompt("How long would you like your password to be? Be sure to pick a number between 8 and 128."));
+  var passwordLength = parseInt(
+    prompt(
+      "How long would you like your password to be? Be sure to pick a number between 8 and 128."
+    )
+  );
   // if clause to check if password length is valid
   // put in clause to restrict to numbers only
-  if (passwordLength < minChars || passwordLength > maxChars) 
-      return alert("Please select a number between 8 and 128."),
-      prompt.passwordLength;
-// PROMPTS CONTINUED...
-  var lowerChars = confirm("Would you like to include lower case letters? \nOK for YES, Cancel for NO");
-  var upperChars = confirm("Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO");
-  var specialChars = confirm("Would you like to include special characters? \nOK for YES, Cancel for NO");
-  var numberChars = confirm("Would you like to include numbers in your password? \nOK for YES, Cancel for NO");
 
-  // store password here
+  console.log(typeof passwordLength);
+
+  if (
+    passwordLength < minChars ||
+    passwordLength > maxChars ||
+    isNaN(passwordLength)
+  ) {
+    console.log(5);
+    alert("Invalid password length entered.");
+    return;
+  }
+
+  // PROMPTS CONTINUED...
+  var isLowerChars = confirm(
+    "Would you like to include lower case letters? \nOK for YES, Cancel for NO"
+  );
+  var isUpperChars = confirm(
+    "Would you like to include UPPERCASE letters? \nOK for YES, Cancel for NO"
+  );
+  var isSpecialChars = confirm(
+    "Would you like to include special characters? \nOK for YES, Cancel for NO"
+  );
+  var isNumberChars = confirm(
+    "Would you like to include numbers in your password? \nOK for YES, Cancel for NO"
+  );
   var password = "";
 
-  // if clause to see if user chose at least one character type:
-  if (!lowerChars && !upperChars && !specialChars && !numberChars) {
-    alert("Please choose at least one character style. We're wasting time, here...");
-    return generatePassword();}
-
-  if (lowerChars || upperChars || specialChars || numberChars)
-      if (lowerChars) {possibleChars.concat(isLower);
-      }
-      if (upperChars) {possibleChars.concat(isUpper);
-      }   if (specialChars) { possibleChars.concat(isSpecial);
-      }   if (numberChars) {possibleChars.concat(isNumber);
-      }
-      console.log(possibleChars);
-      for (i=0; i<passwordLength; i++) {
-      password = password.concat( getRandom(possibleChars));
-     }
-    return password
+  if (isLowerChars) {
+    password += isLower[Math.floor(Math.random() * isLower.length)];
   }
-  
-  function getRandom(arr) {
-    var randIndex = Math.floor(Math.random() * arr.length);
-    var randElement = arr[randIndex];
-    return randElement;
-  }  
-    
-    function writePassword() {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-  
-      passwordText.value = password;
+  if (isUpperChars) {
+    password += isUpper[Math.floor(Math.random() * isUpper.length)];
+  }
+  if (isSpecialChars) {
+    password += isSpecial[Math.floor(Math.random() * isSpecial.length)];
+  }
+  if (isNumberChars) {
+    password += isNumber[Math.floor(Math.random() * isNumber.length)];
+  }
+
+  // if clause to see if user chose at least one character type:
+  if (!isLowerChars && !isUpperChars && !isSpecialChars && !isNumberChars) {
+    alert("Please choose at least one character style.");
+    return;
+  }
+  console.log(1);
+  // if (lowerChars || upperChars || specialChars || numberChars) {
+  if (isLowerChars) {
+    possibleChars += isLower;
+  }
+  if (isUpperChars) {
+    possibleChars += isUpper;
+  }
+  if (isSpecialChars) {
+    possibleChars += isSpecial;
+  }
+  if (isNumberChars) {
+    possibleChars += isNumber;
+  }
+  console.log(possibleChars);
+  var currentPasswordLength = password.length;
+
+  for (i = 0; i < passwordLength - currentPasswordLength; i++) {
+    password += possibleChars[Math.floor(Math.random() * possibleChars.length)];
+    // console.log(possibleChars.length);
+  }
+  console.log(password);
+  return password;
 }
 
-generateBtn.addEventListener("click", generatePassword);
+console.log(2);
+
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  console.log(randIndex);
+  return randElement;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
